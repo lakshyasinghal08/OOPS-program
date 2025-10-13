b07fe28edbbeaba2eb216652df6572c5c0f552e3
Lakshya Singhal <lakshya@example.com>
2025-10-13 16:51:37 +0530

    class

--- first.py
+++ first.py
@@ -149,14 +149,79 @@
 # check_palindrome("lakshya")
 
 # queestion4.
-def fibonacci():
-    n = int(input("enter the number :"))
-    a = 0
-    b = 1
-    for i in range(1,n+1):
-        print(a)
-        c = a + b
-        a = b
-        b = c       
+# def fibonacci():
+#     n = int(input("enter the number :"))
+#     a = 0
+#     b = 1
+#     for i in range(1,n+1):
+#         print(a)
+#         c = a + b
+#         a = b
+#         b = c       
         
-fibonacci()
+# fibonacci()
+# class student:
+#     a = 10
+#     b = 19
+#     sum = a + b
+         
+         
+#     print("sum :",sum)
+
+# class student :
+#     print("lakshya")
+#     def __init__(self,name ,age):
+#         self.name =name
+#         self.age =age
+        
+# s1 = student("aman" , 19)
+# print(s1.name , s1.age)
+
+# s2  = student("vikas", 23)
+# print(s2.name)
+# print(s2.age)
+
+# class employee :
+#     def __init__(self,name , id , salary):
+#         self.name =name
+#         self.id =id
+#         self.salary = salary
+#     def display(self):
+#         print("name =",self.name)
+#         print("id =",self.id)
+#         print("salary =",self.salary)
+            
+# c1 = employee("lakshya" ,1 ,100000)
+# c1.display()
+
+# c2 = employee("vicky" ,2 ,100000)
+# c2.display()
+
+# c3 = employee("singhal" , 3 , 100000)
+# c3.display()
+
+class bank :
+    print("ft : Banking system :")
+    def __init__(self ,name):
+        self.name  = name
+
+    def total_amount(self, amount):
+          self.amount = amount
+    
+    def debit(self , debit ):        
+        self.debit = debit
+        self.amount = self.amount - self.debit
+        print("amount  = ",self.amount)
+       
+        
+    def credit(self ,credit  ):
+        self.credit = credit
+        self.amount = self.amount + self.credit
+        print("amount =",self.amount)
+        
+        
+    
+c1 = bank("lakshya")
+c1.total_amount(100000) 
+c1.debit(400)
+c1. credit(199)
